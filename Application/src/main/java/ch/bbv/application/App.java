fb98304314066b62ef54241588fd39c67d9007dd
Andreas Wassmer <menkalinan56@gmail.com>
2017-03-01 16:38:49 +0100

    Removed now unused imports.

--- Application/src/main/java/ch/bbv/application/App.java
+++ Application/src/main/java/ch/bbv/application/App.java
@@ -1,11 +1,5 @@
 package ch.bbv.application;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-
 /**
  * Hello world!
  *
@@ -14,18 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        try {
-            MqttClient client;
-            
-            client = new MqttClient("tcp://m2m.eclipse.org:1883", "FF22264E2");
-            client.connect();
-            MqttMessage message = new MqttMessage();
-            message.setPayload("drums".getBytes());
-            client.publish("fiddlerOnTheRoof/instrument", message);
-            client.disconnect();
-            System.out.println( "Hello World!" );
-        } catch (MqttException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        MqttHarvester harvester = new MqttHarvester();
+        Thread harvesterThread = new Thread(harvester);
+        harvesterThread.run();
+        
     }
 }
