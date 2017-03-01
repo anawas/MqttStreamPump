24cfc11a71ad754acedf76370d8fc8a23a4ce1e1
Andreas Wassmer <menkalinan56@gmail.com>
2017-03-01 15:55:44 +0100

    Can now publish to m2m.eclipse.org. ** Only publish sample data **

--- Application/src/main/java/ch/bbv/application/App.java
+++ Application/src/main/java/ch/bbv/application/App.java
@@ -1,5 +1,11 @@
 package ch.bbv.application;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            MqttClient client;
+            
+            client = new MqttClient("tcp://m2m.eclipse.org:1883", "FF22264E2");
+            client.connect();
+            MqttMessage message = new MqttMessage();
+            message.setPayload("drums".getBytes());
+            client.publish("fiddlerOnTheRoof/instrument", message);
+            client.disconnect();
+            System.out.println( "Hello World!" );
+        } catch (MqttException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
